8d9e86891cede06c5c88c17241c41acd2df54037
megzcalvert <calvertmegz@gmail.com>
2019-02-14 11:49:54 -0600

    Initial commit

---
+++ HypothesisFour.R
@@ -0,0 +1,16 @@
+rm(list = objects()); ls()
+
+library(readr)
+library(data.table)
+library(tidyverse)
+library(janitor)
+library(tidylog)
+library(broom)
+library(Hmisc)
+library(psych)
+
+getwd()
+setwd("~/Dropbox/Research_Poland_Lab/AM Panel/")
+
+pheno17<- fread("./Phenotype_Database/phenoMatrix17.txt")
+pheno18<- fread("./Phenotype_Database/phenoMatrix18.txt")
