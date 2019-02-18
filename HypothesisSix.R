1747bcee426bb6140d7a283952fa62e08c7cbbda
megzcalvert <calvertmegz@gmail.com>
2019-02-18 10:30:15 -0600

    Graph adjust

--- HypothesisSix.R
+++ HypothesisSix.R
@@ -17,12 +17,12 @@ pheno18<- fread("./Phenotype_Database/pheno18_htpLong.txt")
 
 colnames(pheno17)
 pheno17<- pheno17 %>% 
-  tidylog::select(-GRYLD,-Variety)
+  tidylog::select(-GRYLD)
 pheno17$Date<- as.Date(pheno17$Date)
 
 colnames(pheno18)
 pheno18<- pheno18 %>% 
-  tidylog::select(-GRYLD,-Variety,-year)
+  tidylog::select(-GRYLD,-year)
 pheno18$Date<- as.Date(pheno18$Date)
 
 pheno17 %>% 
@@ -46,3 +46,39 @@ pheno18 %>%
   facet_wrap( ~ID, scales = "free",ncol = 2) + 
   theme_bw() +
   theme(legend.position = "none")
+
+#check
+trial<- pheno17 %>% 
+  tidylog::select(-Plot_ID) %>% 
+  filter(ID == "NDVI") %>% 
+  tidylog::select(-ID) 
+trial$Date<- as.factor(trial$Date)
+fit.lm<- lm(trial$value ~ trial$Date + trial$Variety + trial$Date:trial$Variety)
+summary(fit.lm)
+tidyFit.lm<- tidy(fit.lm)
+glance(fit.lm)
+fit.anova<- anova(fit.lm)
+tidy(fit.anova)
+
+pheno17$Date<- as.Date(pheno17$Date)
+nested17<- pheno17 %>% 
+  tidylog::select(-Plot_ID) %>% 
+  group_by(ID) %>% 
+  nest() %>% 
+  mutate(regression = 
+           map(data, 
+               ~lm(.x$value ~ .x$Date + .x$Variety + .x$Date:.x$Variety)),
+         Res = map(regression,tidy)) %>% 
+  unnest(Res)
+
+pheno18$Date<- as.Date(pheno18$Date)
+nested18<- pheno18 %>% 
+  tidylog::select(-Plot_ID) %>% 
+  group_by(ID) %>% 
+  nest() %>% 
+  mutate(regression = 
+           map(data, 
+               ~lm(.x$value ~ .x$Date + .x$Variety + .x$Date:.x$Variety)),
+         Res = map(regression,tidy)) %>% 
+  unnest(Res)
+
