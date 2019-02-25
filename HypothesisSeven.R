3ad574485c7477439813b13b6f43fcb53bb5bccf
megzcalvert <calvertmegz@gmail.com>
2019-02-25 14:16:38 -0600

    Initial starting model

--- HypothesisSeven.R
+++ HypothesisSeven.R
@@ -23,22 +23,52 @@ phenoLong<- phenoLong %>%
   rename(Plot_ID = entity_id) %>% 
   tidylog::select(Plot_ID,Variety,block,rep,range,column)
 
+pheno17$Date<- as.Date(pheno17$Date,format = "%Y-%m-%d")
+pheno17$Date<- format(pheno17$Date, "%d%b")
+pheno18$Date<- as.Date(pheno18$Date,format = "%Y-%m-%d")
+pheno18$Date<- format(pheno18$Date, "%d%b")
+
 pheno17<- pheno17 %>% 
+  unite("ID",c("ID","Date")) %>% 
   spread(key = ID, value = value) %>% 
-  tidylog::select(Plot_ID,Date,Variety,GRYLD,GNDVI,
-                  GRVI,NDRE,NDVI,NIR,RedEdge) %>% 
+  tidylog::select(Plot_ID,Variety,GRYLD,
+                  GNDVI_02Jun:RedEdge_31Mar) %>% 
   tidylog::inner_join(phenoLong) %>% 
+  glimpse() %>% 
+  distinct() %>% 
   glimpse()
 
 pheno18<- pheno18 %>% 
+  rename(Plot_ID = entity_id)  %>% 
+  unite("ID",c("ID","Date")) %>% 
   spread(key = ID, value = value) %>% 
-  rename(Plot_ID = entity_id, NIR = Nir, RedEdge = RE)  %>% 
-  tidylog::select(Plot_ID,Date,Variety,GRYLD,GNDVI,
-                  GRVI,NDRE,NDVI,NIR,RedEdge) %>% 
+  tidylog::select(Plot_ID,Variety,GRYLD,
+                  GNDVI_04Apr:RE_29May) %>% 
   tidylog::inner_join(phenoLong) %>% 
+  glimpse() %>% 
+  distinct() %>% 
   glimpse()
 
 ###############################################################################
 ####                  ASREML to calculate heritability                  ####
 
+pheno17$Plot_ID<- as.factor(pheno17$Plot_ID)
+pheno17$Variety<- as.factor(pheno17$Variety)
+pheno17$block<- as.factor(pheno17$block)
+pheno17$rep<- as.factor(pheno17$rep)
+pheno17$range<- as.factor(pheno17$range)
+pheno17$column<- as.factor(pheno17$column)
 
+pheno18$Plot_ID<- as.factor(pheno18$Plot_ID)
+pheno18$Variety<- as.factor(pheno18$Variety)
+pheno18$block<- as.factor(pheno18$block)
+pheno18$rep<- as.factor(pheno18$rep)
+pheno18$range<- as.factor(pheno18$range)
+pheno18$column<- as.factor(pheno18$column)
+
+#2017 trial
+# Without the auto-reggression correlation
+
+t17<- asreml(fixed = GRYLD ~ 1,
+             random = ~block,
+             data = pheno17)
